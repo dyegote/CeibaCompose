062495da553af61c27265a098f2077ba53a8c526
Diego Tobar <dyegote26@gmail.com>
2021-11-29 18:50:17 -0500

    ajustes

--- app/src/main/java/com/dt/composeapp/viewmodel/Builders.kt
+++
@@ -1,8 +0,0 @@
-package com.dt.composeapp.viewmodel
-
-import kotlinx.coroutines.flow.FlowCollector
-import kotlin.experimental.ExperimentalTypeInference
-
-@OptIn(ExperimentalTypeInference::class)
-fun <T> resourceFlow(@BuilderInference block: suspend FlowCollector<T>.() -> Unit): ResourceFlow<T> = ResourceFlow(block)
-
